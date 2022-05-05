10b918da659b8dcac8f7c8540f91fd5b43c0a12b
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-05 13:09:17 -0400

    Creados módulos, definido layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'foo', loadChildren: () => import('./foo/foo.module').then(m => m.FooModule) },
+  { path: 'bar', loadChildren: () => import('./bar/bar.module').then(m => m.BarModule) },
+  { path: 'baz', loadChildren: () => import('./baz/baz.module').then(m => m.BazModule) },
+  { path: '', redirectTo:'/foo', pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  menuOptions: Array<any> = [
+    { route: 'foo', name: 'Foo' },
+    { route: 'bar', name: 'Bar' },
+    { route: 'baz', name: 'Baz' }
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
